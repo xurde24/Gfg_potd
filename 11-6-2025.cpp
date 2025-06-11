3badf96f06eb3801fc30379dd872b5ee8467cc62
Shreyansh <anandshreyansh24@gmail.com>
2025-06-11 21:20:07 +0530

    11 june task updated

---
+++ 11-6-2025.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int findLength(vector<int> &color, vector<int> &radius) {
+        stack<pair<int,int>> st;
+        int n = color.size();
+        
+        for(int i=0;i<n;++i)
+        {
+            int c = color[i];
+            int r = radius[i];
+            
+            if(!st.empty() && st.top().first == c && st.top().second == r)
+                st.pop();
+            else
+                st.push({c,r});
+        }
+        
+        return st.size();
+    }
+};

ece4195813104a08fe730b032e500fd3d6b72985
Shreyansh Anand <anandshreyansh24@gmail.com>
2025-09-06 23:06:57 +0530

    6 sept code added

---
+++ 06-09-2025.cpp
@@ -0,0 +1,45 @@
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        if(!head) return head;
+        
+        Node* zeroDummy = new Node(-1);
+        Node* oneDummy = new Node(-1);
+        Node* twoDummy = new Node(-1);
+        
+        Node* zero = zeroDummy;
+        Node* one = oneDummy;
+        Node* two = twoDummy;
+        
+        Node* temp = head;
+        
+        while(temp){
+            if(temp->data == 0){
+                zero->next = temp;
+                zero = zero->next;
+            } else if(temp->data == 1){
+                one->next = temp;
+                one = one->next;
+            } else {
+                two->next = temp;
+                two = two->next;
+            }
+            temp = temp->next;
+        }
+        
+        
+        zero->next = (oneDummy->next) ? oneDummy->next : twoDummy->next;
+        one->next = twoDummy->next;
+        two->next = nullptr; 
+        
+        Node* result = zeroDummy->next;
+        
+        
+        delete zeroDummy;
+        delete oneDummy;
+        delete twoDummy;
+        
+        return result;
+    }
+};
+
